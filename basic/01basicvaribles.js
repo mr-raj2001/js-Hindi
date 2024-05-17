fb92f8046d9bea64845fb04a8964c606951b5cc0
Rishav Raj <79185086+mr-raj2001@users.noreply.github.com>
2024-05-17 05:37:50 +0000

    datatypes

---
+++ basic/01basicvaribles.js
@@ -0,0 +1,19 @@
+const accountId = 144567
+let accountEmail = "bla@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+
+// accountId = 2
+//the above thing is not allowed
+
+accountEmail = "raj@gamil.com"
+accountPassword = "3848934"
+accountCity = "Bengaluru"
+
+
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
+
---
+++ basic/02datatypes.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//alert(3 + 3) // we are using node js not browser
+
+//number = 2 to power 53 range
+//bignint
+//string ""
+//boolean = simple yes or no
+//null=> it is a stand alone value but also a data type
+
+//udefined => its different from null
+
+//symbol uniquness find karne ke kam ata he like components in figma 
+
+
+//object 
+
+
+console.log(typeof null);   //object ayega 
+
+//undefined ka type undefined he
